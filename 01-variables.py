5a6466d76060ffbc03be0ad20b7cffdfc6988227
Vicente Garcia <byvichooo10@users.noreply.github.com>
2023-04-06 16:09:26 -0400

    Mi primer commit

---
+++ 01-variables.py
@@ -0,0 +1,26 @@
+print("Hola")
+#Este es un comentario
+
+#Dclarando una variable
+nombre = "Vicente"
+
+#Impresion de una variable
+print(nombre)
+print("Hola soy", nombre)
+
+#Declarando segundo variable
+edad = 18
+
+print("tengo", edad , "años")
+
+#print("Hola mi nombre es" + nombre + "y tengo" + str(edad) + "años")
+print("Hola mi nombre es" + " " + nombre + " " + "y tengo" + " " + str(edad) + " " + "años")
+
+#nombre = "Vicho"
+#print("Hola mi nuevo nombre es", nombre)
+
+#Utilizando la instruccion input
+#nombre = input("¿Cual es tu nombre?\n")
+#print("Tu nombre es:",nombre)
+
+print("Hola me llamo" + " " + nombre + " " + "Mi edad es de" + " " + str(edad) + " " + "Años")
---
+++ 02-tiposdedatos.py
@@ -0,0 +1,28 @@
+#01-DATOS DE TIPO NUMERICO
+estatura = 1.73
+peso = 60
+complejo = 1 +4j
+
+print("Impresion del numero complejo:",complejo)
+
+#OPERACION ARITMETICA BASICA
+imc = peso/estatura**2
+print("Mi imc es de:" ,imc)
+
+#02- DATOS DE TIPO DE CADENA
+asignatura = "Programacion" 
+carrera = "Ingenieria civil en informatica"
+print("Mi carrera es",carrera, "y estoy en la asignatura de",asignatura)
+
+#03- VALORES BOOLEANOS
+ampolleta = False
+interruptor = True
+
+#CON TYPE SABEMOS EL TIPO DE DATOS QUE ESTAMOS TRATANDO
+print(type(ampolleta))
+
+#04- DATOS DE TIPOS ARRAY (OBJETOS DE TIPO COLECCION)
+estudiantes = ["Vicente", "Matias", "Carlos"]
+print(estudiantes)
+num = [1,2,3,4,5,6]
+print(num)
