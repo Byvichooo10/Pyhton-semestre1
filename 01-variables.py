053129d0054708e6041b60605edfb4e9fdea1fa8
Vicente Garcia <byvichooo10@users.noreply.github.com>
2023-04-11 15:49:26 -0400

    Nuevos cambios

--- 01-variables.py
+++ 01-variables.py
@@ -20,7 +20,11 @@ print("Hola mi nombre es" + " " + nombre + " " + "y tengo" + " " + str(edad) + "
 #print("Hola mi nuevo nombre es", nombre)
 
 #Utilizando la instruccion input
-#nombre = input("¿Cual es tu nombre?\n")
-#print("Tu nombre es:",nombre)
+nombre = input("¿Cual es tu nombre?\n")
+print("Tu nombre es:",nombre)
 
 print("Hola me llamo" + " " + nombre + " " + "Mi edad es de" + " " + str(edad) + " " + "Años")
+
+#06- VARIABLES EN UNA SOLA LINEA
+ciudad, region, pais, year = "Osorno", "Los lagos", "Chile", "2023"
+print("Yo naci en ", ciudad, pais)
--- 02-tiposdedatos.py
+++ 02-tiposdedatos.py
@@ -9,6 +9,8 @@ print("Impresion del numero complejo:",complejo)
 imc = peso/estatura**2
 print("Mi imc es de:" ,imc)
 
+print("mi imc es de {:.2f}".format(imc))
+
 #02- DATOS DE TIPO DE CADENA
 asignatura = "Programacion" 
 carrera = "Ingenieria civil en informatica"
@@ -22,7 +24,45 @@ interruptor = True
 print(type(ampolleta))
 
 #04- DATOS DE TIPOS ARRAY (OBJETOS DE TIPO COLECCION)
-estudiantes = ["Vicente", "Matias", "Carlos"]
+estudiantes = ["Vicente", "Matias", "Carlos", "Andres"]
 print(estudiantes)
-num = [1,2,3,4,5,6]
+num = [1,2,3,4,5,6,2]
 print(num)
+lenguaje = ["Pyhton"]
+
+#DECLARANDO O INICIANDO LISTAS
+nueva_lista = list()
+print("Esta es una lista vacia:",nueva_lista)
+
+otra_lista = []
+print("Esta es otra lista vacia",otra_lista)
+print(type(otra_lista))
+
+print("Lista de numeros:", num)
+print(len(num))
+print(num.count(2))
+
+#Como accedo a un elemento en especifico de la lista?
+print(estudiantes[0]) #0 siempre es primer elemento de la lista
+print(estudiantes[1])
+print(estudiantes[3])
+print(estudiantes[-2]) #Se invierten posiciones
+
+#Sobreescribir otro dato
+estudiantes[2] = "Benjamin"
+print("Los estudiantes son:", estudiantes)
+
+#INICIALIZANDO OTRA LISTA DE DATOS MIXTOS
+data_asig = [10023,"Programacion",1,True]
+#Que hace este codigo?
+cod, ramo, semestre, estado = data_asig
+print(ramo)
+print(cod)
+
+#Se pueden sumar listas?
+print("Suma de listas", estudiantes + num)
+
+print(list("Python"))
+print(list(range(10)))
+print("\n")
+print(list("Benjamin"))
