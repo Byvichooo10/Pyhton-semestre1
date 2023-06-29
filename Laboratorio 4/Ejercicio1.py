660342469c4075a54088db6712cff76f692f97c7
Vicente Garcia <byvichooo10@users.noreply.github.com>
2023-06-28 20:41:34 -0400

    Vicente

---
+++ Laboratorio 4/Ejercicio1.py
@@ -0,0 +1,7 @@
+trabajadores = [ ["Juan", [700000, 650000, 690000] ], ["María", [681000, 710000, 590000] ],
+["Pedro", [590000, 635000, 705000] ] ]
+
+# A) Implementar una función que calcule el promedio del sueldo de cada trabajador. La función debe recibir como parámetro la lista de sueldo del trabajador y devolver el promedio. El promedio tiene que tener 2 decimales como salida.
+
+def calcular_promedio(sueldos):
+    sueldos
