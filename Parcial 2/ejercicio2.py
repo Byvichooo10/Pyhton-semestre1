aff2b032f534897c66635df515015708e10cf0b8
Vicente Garcia <byvichooo10@users.noreply.github.com>
2023-06-28 20:53:17 -0400

    Vicente

--- Parcial 2/ejercicio2.py
+++ Parcial 2/ejercicio2.py
@@ -3,30 +3,23 @@ ancho_base = 19
 numero = 1
 incremento = 1
 
-
 espacios = (ancho_base - 1) // 2
-
 
 for i in range(1, altura+1):
     
     for j in range(espacios):
         print(" ", end="")
-    
    
     for k in range(2*i - 1):
         print(numero, end="")
         numero = (numero + incremento) % 10
     
- 
     for j in range(espacios):
         print(" ", end="")
-    
 
     print()
-    
  
     espacios -= 1
-
     numero = 9 if incremento == 1 else 0
     incremento = -1 if incremento == 1 else 1
 
