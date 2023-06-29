fafb1022b8227eeb7ca6f4a1b5ed4f19729d8177
Vicente Garcia <byvichooo10@users.noreply.github.com>
2023-06-28 20:39:57 -0400

    Vicente

--- Laboratorio 4/Ejercicio2.py
+++ Laboratorio 4/Ejercicio2.py
@@ -4,8 +4,30 @@ c = [20,35,60,90,20]
 
 # A) Crear una función para encontrar los valores en común de las tres listas
 
-def valores_comunes():
-    valores_comun = set(a) & set(b) & set (c)
-    return list(valores_comun)
+def valores_comunes(a,b,c):
+    return list(set(a) & set(b) & set(c))
 
-print(valores_comunes)
+# B) Crear una función para concatenar todas las listas en una sola.
+
+def lista_total(a, b, c):
+   # lista_total = concatenar(a + b + c)
+    return lista_total
+
+# C) Crea una función y eliminar los valores duplicados de la lista anterior.
+
+def valores_duplicados(a,b,c):
+    lista_ordenada = set(lista_total)
+    lista_organizada = list(lista_ordenada)
+    return lista_organizada
+
+# D) Crear una función y ordenar la lista de forma descendente.
+
+def lista_descendente(a,b,c):
+   # lista_descendente = lista_organizada(reverse=True)
+    return lista_descendente
+
+# E) Crear una función y reemplazar el valor de la posición 7° por el número 100
+
+def remplazar_pos(a,b,c):
+    lista_remplazada = lista_descendente(6,100)
+    return lista_remplazada
